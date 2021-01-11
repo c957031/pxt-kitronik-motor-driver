ee57f51cd0523483e99ca0c98f96b13e430c858e
c957031 <77271940+c957031@users.noreply.github.com>
2021-01-11 11:29:11 +0100

    Update pxt.json, main.ts, main.blocks

--- main.ts
+++ main.ts
@@ -1,6 +1,5 @@
 let Lys = 0
-input.onButtonPressed(Button.A, function on_button_pressed_a() {
-    
+input.onButtonPressed(Button.A, function () {
     while (true) {
         Lys = pins.analogReadPin(AnalogPin.P1)
         if (Lys > 500) {
@@ -10,20 +9,24 @@ input.onButtonPressed(Button.A, function on_button_pressed_a() {
             kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor1, kitronik_motor_driver.MotorDirection.Reverse, 40)
             kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor2, kitronik_motor_driver.MotorDirection.Reverse, 40)
             basic.pause(1000)
-            kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor1, kitronik_motor_driver.MotorDirection.Forward, 80)
-            kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor2, kitronik_motor_driver.MotorDirection.Reverse, 80)
+            if (randint(0, 1) == 1) {
+                kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor1, kitronik_motor_driver.MotorDirection.Reverse, 80)
+                kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor2, kitronik_motor_driver.MotorDirection.Forward, 80)
+            } else {
+                kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor1, kitronik_motor_driver.MotorDirection.Forward, 80)
+                kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor2, kitronik_motor_driver.MotorDirection.Reverse, 80)
+                basic.pause(randint(450, 1450))
+            }
             basic.pause(randint(450, 1450))
         }
-        
         basic.pause(10)
     }
 })
-input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
-    
+input.onButtonPressed(Button.AB, function () {
     Lys = pins.analogReadPin(AnalogPin.P1)
     basic.showNumber(Lys)
 })
-input.onButtonPressed(Button.B, function on_button_pressed_b() {
+input.onButtonPressed(Button.B, function () {
     kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor1, kitronik_motor_driver.MotorDirection.Forward, 80)
     kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor2, kitronik_motor_driver.MotorDirection.Reverse, 80)
     basic.pause(1350)
